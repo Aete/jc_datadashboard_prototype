f4aef7e7e03502297f2fc07368c4de6e7fa41ef4
Aete <hsg8234@naver.com>
2019-11-04 10:09:10 -0500

    Prototype of JCgreen-dashboard
    - I am still designing prototype of this dashboard....

---
+++ js/data/dataset1.js
@@ -0,0 +1,14 @@
+var dataset_jc_green = {
+    'Downtown':{'rain':0.10,'lat':40.723850},
+    'McGinley Square': {"rain":0.15,"lat":40.72199859608176},
+    'Journal Square': {"rain":0.20,"lat":40.73439997867336},
+    'The Heights': {"rain":0.10,"lat":40.746501999169425},
+    'The Waterfront': {"rain":0.10,"lat":40.723850},
+    'West Side': {"rain":0.10,"lat":40.71983169791139},
+    'Hackensack Riverfront': {"rain":0.10,"lat":40.7249412311738},
+    'Liberty Park': {"rain":0.10,"lat":40.69923748533216},
+    'Greenville': {"rain":0.10,"lat":40.7009381205886},
+    'Bergen-Lafayette':{"rain":0.10,"lat":40.71241324924449},
+    'Lincoln Park': {"rain":0.30,"lat":40.728145292890616},
+    'Jersey City': {"rain":1.05,"lat":40.728145292890616}
+};
--- js/map.js
+++ js/map.js
@@ -6,30 +6,44 @@ var map = new mapboxgl.Map({
 });
 map.addControl(new mapboxgl.NavigationControl(), 'top-left');
 
-
-var url1 = 'js/data/jerseycity.json';
-var url2 = 'js/data/neighborhoods.geojson';
+var url_neighborhood = 'js/data/neighborhoods.geojson';
+var url_park = 'js/data/park.geojson';
 var hoverNeighborhoodId =  null;
+var clickNeighborhoodId = null;
+var neigh_clicked = '';
+var neigh_clicked_now = '';
+var neigh_hovered = '';
+var coord = '';
+var centerpoints = {
+    'Downtown':{'lng':-74.040664,'lat':40.723850},
+    'McGinley Square': {"lng":-74.06397302244267,"lat":40.72199859608176},
+    'Journal Square': {"lng":-74.06052908133006,"lat":40.73439997867336},
+    'The Heights': {"lng":-74.04084950732488,"lat":40.746501999169425},
+    'The Waterfront': {"lng":-74.026000,"lat":40.723850},
+    'West Side': {"lng":-74.0750483934907,"lat":40.71983169791139},
+    'Hackensack Riverfront': {"lng":-74.07715286495596,"lat":40.7249412311738},
+    'Liberty Park': {"lng":-74.04780803472653,"lat":40.69923748533216},
+    'Greenville': {"lng":-74.0800702031342,"lat":40.7009381205886},
+    'Bergen-Lafayette':{"lng":-74.05639110356594,"lat":40.71241324924449},
+    'Lincoln Park': {"lng":-74.0789477476726,"lat":40.728145292890616}
+};
 
 map.on('load', function () {
     window.setInterval(function() {
-        map.getSource('route').setData(url1);
-        map.getSource('neighborhood').setData(url2);
+        map.getSource('park').setData(url_park);
+        map.getSource('neighborhood').setData(url_neighborhood);
     }, 2000);
-    map.addSource('route', { type: 'geojson', data: url1, 'generateId': true});
-    map.addSource('neighborhood', { type: 'geojson', data: url2, 'generateId': true});
-
+    map.addSource('park', { type: 'geojson', data: url_park, 'generateId': true});
+    map.addSource('neighborhood', { type: 'geojson', data: url_neighborhood, 'generateId': true});
     map.addLayer({
-        "id": "Jersey city",
-        "source": "route",
-        "type": "line",
+        "id": "park",
+        "source": "park",
+        "type": "fill",
         "layout": {
-            "line-join": "round",
-            "line-cap": "round"
         },
         "paint": {
-            "line-color": "blue",
-            "line-width": 1
+            "fill-color": "Green",
+            "fill-opacity": 0.4
         }
     });
 
@@ -42,8 +56,14 @@ map.on('load', function () {
             "line-cap": "round"
         },
         "paint": {
-            "line-color": "black",
-            "line-width": 1
+            "line-color": ["case",
+                ["boolean", ["feature-state", "click"], false],
+                "Green",
+                "white"
+
+            ] ,
+            "line-width": 1.5,
+            "line-dasharray": [0.1, 2.5]
         }
     });
 
@@ -53,7 +73,7 @@ map.on('load', function () {
         "type": "fill",
         "layout": { },
         "paint": {
-            "fill-color": "green",
+            "fill-color": "#a6a6a6",
             "fill-opacity": ["case",
                 ["boolean", ["feature-state", "hover"], false],
                 0.2,
@@ -61,14 +81,32 @@ map.on('load', function () {
             ]
         }
     });
+
+    map.on("click", "neighborhoods-border",  function(e) {
+        if (e.features.length > 0) {
+            if (hoverNeighborhoodId) {
+                map.setFeatureState({source: 'neighborhood', id: clickNeighborhoodId}, { click: false});
+            }
+            neigh_clicked_now = e.features[0].properties.NAME;
+            clickNeighborhoodId = e.features[0].id;
+            map.setFeatureState({source: 'neighborhood', id: 0}, {click: false});
+            if(neigh_clicked === neigh_clicked_now){
+                map.setFeatureState({source: 'neighborhood', id: 0}, {click: false});
+            }
+            else{
+                map.setFeatureState({source: 'neighborhood', id: clickNeighborhoodId}, {click: true});
+            }
+
+    }});
+
     map.on("click", "neighborhoods-fill", function(e) {
-        var neighName = e.features[0].properties.NAME;
-        console.log(e.features[0]);
-        $("#test").text(neighName);
+        neigh_clicked = e.features[0].properties.NAME;
+
+        $("#test").text(neigh_clicked);
+        $("#rain").text(dataset_jc_green[neigh_clicked]['rain']);
         map.flyTo({
-            center: [ -74.030000, 40.720935],
-            zoom: 14
-        });
+            center: [ centerpoints[neigh_clicked]['lng'], centerpoints[neigh_clicked]['lat']],
+            zoom: 14})
     });
 
     map.on("mousemove", "neighborhoods-fill", function(e) {
@@ -76,9 +114,15 @@ map.on('load', function () {
             if (hoverNeighborhoodId) {
                 map.setFeatureState({source: 'neighborhood', id: hoverNeighborhoodId}, { hover: false});
             }
+            neigh_hovered = e.features[0].properties.NAME;
             hoverNeighborhoodId = e.features[0].id;
             map.setFeatureState({source: 'neighborhood', id: 0}, { hover: false});
-            map.setFeatureState({source: 'neighborhood', id: hoverNeighborhoodId}, { hover: true});
+            if(neigh_clicked ===neigh_hovered){
+                map.setFeatureState({source: 'neighborhood', id: 0}, { hover: false});
+            }
+            else{
+                map.setFeatureState({source: 'neighborhood', id: hoverNeighborhoodId}, { hover: true});
+            }
         }
     });
 
